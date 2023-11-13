0ab334fee8756c4101a7c547fd1137708acdb161
VirgiD <virginiadanitz@gmail.com>
2023-11-13 10:31:28 -0300

    utlimos cambios

--- src/main/java/com/ar/cac/homebanking/models/User.java
+++ src/main/java/com/ar/cac/homebanking/models/User.java
@@ -16,7 +16,6 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
 
     @Column(name ="nombre")
--- src/main/java/com/ar/cac/homebanking/models/dtos/UserDTO.java
+++ src/main/java/com/ar/cac/homebanking/models/dtos/UserDTO.java
@@ -5,12 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class UserDTO {
+
+    private Long id;
 
     private String name;
 
