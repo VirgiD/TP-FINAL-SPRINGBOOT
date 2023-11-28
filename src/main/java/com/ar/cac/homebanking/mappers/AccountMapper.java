c39bce65685e19462f80e5ad4c7a91bd4327395f
VirgiD <virginiadanitz@gmail.com>
2023-11-28 14:56:57 -0300

    cambios Account

--- src/main/java/com/ar/cac/homebanking/mappers/AccountMapper.java
+++ src/main/java/com/ar/cac/homebanking/mappers/AccountMapper.java
@@ -1,4 +1,15 @@
 package com.ar.cac.homebanking.mappers;
 
+import com.ar.cac.homebanking.models.Account;
+import com.ar.cac.homebanking.models.dtos.AccountDTO;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class AccountMapper {
+    //TODO : Refactor builder
+    public AccountDTO accountToDTO(Account acount){
+    AccountDTO dto = new AccountDTO();
+    return dto;
+}
+
 }
